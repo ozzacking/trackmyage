bc47426306b79b2dbd9fdf776cb2b622746437b3
ozzacking <ozzacking@gmail.com>
2026-05-22 03:19:10 -0700

    Update BrowserRouter basename to '/trackmyage'

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Recommended from './pages/Recommended';
 
-const BASE = import.meta.env.BASE_URL;
-
 export default function App() {
   return (
-    <BrowserRouter basename={BASE}>
+    <BrowserRouter basename="/trackmyage">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recommended" element={<Recommended />} />
